c5e5072d0657d1051bf8511a459f94f6bca954cd
Bright Anyawe <brightanyawe55@gmail.com>
2024-11-02 22:31:11 +0000

    Refactor: address error property in route

--- .history/src/Components/Layout/ErrorPage_20241026114759.jsx
+++ .history/src/Components/Layout/ErrorPage_20241026114759.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const ErrorPage = () => {
   return (
     <div>
-      <h1>Oh no, this route does not exist!</h1>
+      <h1>Oh no, this route doesn&apos;t exist!</h1>
       <Link to="/">
         You can go back to the home page by clicking here, though!
       </Link>
